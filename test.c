dc2298863c95546b991fd2d9ae0ef0cd19fbbaaf
Zhou Kaize <zhoukaize232@163.com>
2024-11-01 21:49:35 +0800

    为test.c文件写入了求阶乘和的功能

--- test.c
+++ test.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <string.h>
+long int fac(int n) // 定义为long int 型，避免溢出
+{
+    long int f;
+    if (n == 0)
+        return 0; // 当n=0是，递归法到尽头，依次返回函数值。
+    f = fac(n - 1) * n;
+    return (f); // 返回最后一次函数值，即单次阶乘的最后结果
+}
+
 int main()
 {
-    int n = 0, a = 0;
-    char str[50];
-    scanf("%d", &n);
-    scanf("%s", str);
-    if (n == 0)
-    {
-        printf("%s", str);
-        return 0;
-    }
-    a = strlen(str);
-    for (int i = 0; i < a; i++)
-    {
-        str[i] = ((str[i] - 'a' + n) % 26) + 'a';
-    }
-    printf("%s", str);
+    long int n;
+    long int s = 0;
+    int i;
+    scanf("%ld", &n); // 此处注意scanf_s与scanf的使用场景
+    for (i = 1; i <= n; i++)
+        s += fac(i); // 以循环控制阶乘的和。fac函数每一次的返回值作为s的自加值
+    printf("%ld", s);
     return 0;
+    
 }
